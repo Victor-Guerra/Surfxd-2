bedd03beecd6fba55c2cfaf56fef82a60056b451
Victor Guerra <wasmon999@hotmail.com>
2019-11-07 10:23:22 -0700

    add no score file case

---
+++ main.py
@@ -0,0 +1,10 @@
+from sys import argv
+form sys import exit
+
+script, score_file = argv
+
+if score_file == null:
+    print("Please run the program again, with the file to use\nas an argument for the program.")
+    exit(0)
+else:
+
